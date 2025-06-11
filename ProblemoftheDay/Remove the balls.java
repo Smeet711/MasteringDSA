97f03950e31c429278172b6f1aa3bc17312a24c7
Smeet711 <smeetgadhiya01@gmail.com>
2025-06-11 22:39:26 +0530

    Problem of the Day 11 June 2025

---
+++ ProblemoftheDay/Remove the balls.java
@@ -0,0 +1,54 @@
+// You are given two arrays, color and radius, representing a sequence of balls:
+
+
+// color[i] is the color of the i-th ball.
+// radius[i] is the radius of the i-th ball.
+
+// If two consecutive balls have the same color and radius, remove them both. Repeat this process until no more such pairs exist.
+
+
+// Return the number of balls remaining after all possible removals.
+
+// Examples:
+
+// Input: color[] = [2, 3, 5], radius[] = [3, 3, 5]
+// Output: 3
+// Explanation: All the 3 balls have different colors and radius.
+// Input: color[] = [2, 2, 5], radius[] = [3, 3, 5]
+// Output: 1
+// Explanation: First ball and second ball have same color 2 and same radius 3. So, after removing only one ball is left. It cannot be removed from the array. Hence, the final array has length 1.
+// Constraints:
+// 1 ≤ color.size() = radius.size() ≤ 105
+// 1 ≤ color[i] ≤ 109
+// 1 ≤ radius[i] ≤ 109                                                                                                                                           
+
+
+class Solution {
+    public int findLength(int[] color, int[] radius) {
+        int n=color.length;
+        Stack<Pair> st=new Stack<>();
+        for(int i=0;i<n;i++)
+        {
+            if(!st.isEmpty() && st.peek().col==color[i] && st.peek().rad==radius[i])
+            {
+                st.pop();
+            }
+            else
+          {  st.push(new Pair(color[i],radius[i]));}
+        }
+       // System.out.println(st);
+        return st.size();
+    }
+}
+class Pair
+{
+    int col;
+    int rad;
+    public Pair(int col,int rad)
+    {
+        this.col=col;
+        this.rad=rad;
+    }
+}
+
+
